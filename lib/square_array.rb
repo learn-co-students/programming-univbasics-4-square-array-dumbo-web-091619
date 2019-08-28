dee40cad68b3843692895269dbc853b41cab8269
Emirhan Kaplan <emskaplann@gmail.com>
2019-08-28 02:44:24 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
+array1 = [1,23,4,5,6,15,23]
 def square_array(array)
-  # your code here
+  i = 0
+  newarray1 = []
+  while i<array.length 
+    newarray1 << array[i]**2
+    i += 1
+  end
+  return newarray1
 end
